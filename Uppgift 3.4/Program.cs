ca8492d7327a24286835ce12add4029bad014950
Dijar06 <dize06001@utb.vaxjo.se>
2023-10-06 12:49:28 +0200

    Add project files.

---
+++ Uppgift 3.4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uppgift3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur lång är din låt i minuter?");
+            int minuter = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur lång är din låt i sekunder?");
+            int sekunder = int.Parse(Console.ReadLine());
+            int totaltid = minuter * 60 + sekunder;
+
+            if (totaltid <= 260 && totaltid >= 165)
+            {
+                Console.WriteLine("Din låt får spelas på radion!");
+            }
+            else
+            {
+                Console.WriteLine("Din låt får ej spelas på radion.");
+            }
+        }
+    }
+}
